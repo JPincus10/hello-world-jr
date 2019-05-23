9775367a1c6cd8db371454a0c77b08cca5705560
evilchickadee <50250797+evilchickadee@users.noreply.github.com>
2019-05-23 19:57:58 -0400

    Update meritbadge.js

--- meritbadge.js
+++ meritbadge.js
@@ -1,5 +1,9 @@
-var differences = 1+1;
+ https://www.w3schools.com/jsref/jsref_random.asp
+var differences = 34.5 + 34.5;
 
 // https://www.w3schools.com/jsref/jsref_random.asp <--- Random numbers
-// print('blah') with js is document.write("Hello world")
+// print('blah') with js is document.write("Helloooooo world")
 // https://jsfiddle.net/ <--- To test your code, just put it in the JavaScript part (Ignore HTML+CSS)
+
+
+Math.floor((Math.random() * 100) + 1);
