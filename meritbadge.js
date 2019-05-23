24af319199fa517943c86258938d78e65e10b37b
Jon <45501389+JonPizza@users.noreply.github.com>
2019-05-23 19:08:10 -0400

    factz

--- meritbadge.js
+++ meritbadge.js
@@ -1,1 +1,5 @@
 var differences = 1+1;
+
+// https://www.w3schools.com/jsref/jsref_random.asp <--- Random numbers
+// print('blah') with js is document.write("Hello world")
+// https://jsfiddle.net/ <--- To test your code, just put it in the JavaScript part (Ignore HTML+CSS)
