09b5bcdf8cd26b9df7f992f4293a2890a8477135
evilchickadee <50250797+evilchickadee@users.noreply.github.com>
2019-05-29 14:52:08 -0400

    Update and rename meritbadge.js to meritbadge.HTML

--- meritbadge.js
+++
@@ -1,9 +0,0 @@
- https://www.w3schools.com/jsref/jsref_random.asp
-var differences = 34.5 + 34.5;
-
-// https://www.w3schools.com/jsref/jsref_random.asp <--- Random numbers
-// print('blah') with js is document.write("Helloooooo world")
-// https://jsfiddle.net/ <--- To test your code, just put it in the JavaScript part (Ignore HTML+CSS)
-
-
-Math.floor((Math.random() * 100) + 1);
